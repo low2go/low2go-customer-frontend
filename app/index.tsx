8d3cbf34f5737dd7d1e5dc9c25256c578046bb1d
Joshua Esrig <joshuaesrig@Joshuas-MacBook-Air.local>
2024-12-01 19:37:57 -0600

    base

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,21 @@
-import { Text, View } from "react-native";
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    // Additional styles for the text can go here
+  },
+});
