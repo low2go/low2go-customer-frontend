7afc9c36c2e8f203f69a2ae083bf4fd349876135
Joshua Esrig <joshuaesrig@Joshuas-MacBook-Air.local>
2024-12-04 19:41:44 -0600

    listview complete

--- components/List/GridItem.tsx
+++ components/List/GridItem.tsx
@@ -1,34 +1,66 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 import { Colors } from '@/constants/colors';
 
 type GridItemProps = {
   productId: string;
   name: string; 
+  stock: number;
+  price: number;
+  imageUrl: string;
 };
 
-const GridItem = ({ name, productId }: GridItemProps) => {
+const GridItem = ({ name, productId, stock, price, imageUrl }: GridItemProps) => {
   return (
     <View style={styles.gridItem}>
-      <Text style={styles.itemText}>{name}</Text>
-      <Text style={styles.itemText}>{productId}</Text>
+      <Image 
+        source={{ uri: imageUrl }} 
+        style={styles.image} 
+        onError={() => console.warn(`Failed to load image: ${imageUrl}`)} 
+      />
+      <View style={styles.textContainer}>
+        <Text style={styles.priceText}>${price}</Text>
+        <Text style={styles.itemText}>{name}</Text>
+        <Text style={styles.itemText}>Stock: {stock}</Text>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   gridItem: {
+    paddingLeft: 15,
+    paddingRight: 15,
     flex: 1,
-    margin: 8,
-    backgroundColor: '#f0f0f0',
-    height: 180,
-    justifyContent: 'center',
-    alignItems: 'center',
+    margin: 5,
+    backgroundColor: 'white',
+    height: 200,
     borderRadius: 8,
+    overflow: 'hidden', // Prevent content overflow
+    flexDirection: 'column', // Stack items vertically
+    alignItems: 'center', // Center content horizontally
+  },
+  image: {
+    top: 3,
+    width: '100%',
+    borderRadius: 10,
+    height: 120,
+    resizeMode: 'contain', // Ensure the image covers the space while maintaining aspect ratio
+  },
+  textContainer: {
+    width: '100%',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    paddingHorizontal: 8,
+  },
+  priceText: {
+    fontSize: 20,
+    color: Colors.secondary,
   },
   itemText: {
-    fontSize: 16,
+    fontSize: 14,
     color: Colors.text,
+    marginTop: 4,
   },
 });
 
